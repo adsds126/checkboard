fcb34b45eab553cc0562168e4c566012034b2cc9
gimtaehyeong <adsds126@gmail.com>
2024-08-25 18:15:55 +0900

    first settings

---
+++ src/main/java/com/adsds126/checkboard/CheckboardApplication.java
@@ -0,0 +1,13 @@
+package com.adsds126.checkboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adsds126/checkboard/CheckboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsds126.checkboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
